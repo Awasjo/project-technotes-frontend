f8432f65df4be496b56c1392587125107aa57b69
Awasjo <awasjomail@gmail.com>
2023-11-01 20:27:25 -0400

    added some components and login welcome page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
-import{}
+import{Routes, Route } from 'react-router-dom';
+import Layout from './components/Layout';
+import Public from './components/Public';
+import Login from './features/auth/Login';
+import DashLayout from './components/DashLayout';
 
 
 
 function App() {
   return (
+    <Routes>
+      {/* here we have the path going to a layout, this is not the index page */}
+      <Route path="/" element={<Layout />}>
+        {/* the below here is the index page  */}
+      <Route index element={<Public />}/> 
+      {/* again below is not an index, its denoted by a path  */}
+      <Route path="login" element={<Login />}/>
 
-  )
+      {/* this is the protected route */}
+      <Route path="dash" element={<DashLayout />}>
+
+      </Route>
+
+      </Route>
+    </Routes>
+  );
 }
 
 export default App;
---
+++ src/components/Layout.js
@@ -0,0 +1,8 @@
+import { Outlet } from "react-router-dom";
+
+//this helps us keep banners headers and such consistant throughout the web application
+const Layout = () => {
+  return <Outlet/>
+}
+
+export default Layout
